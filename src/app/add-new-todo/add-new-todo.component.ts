7ed5a644321c1559d64c7c4ab9e2b0256a4656a1
Saman      keighobadi <61837740+SamanKeighobadi@users.noreply.github.com>
2023-04-04 21:22:15 +0430

    feat: add new todo component

---
+++ src/app/add-new-todo/add-new-todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-new-todo',
+  templateUrl: './add-new-todo.component.html',
+  styleUrls: ['./add-new-todo.component.css']
+})
+export class AddNewTodoComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddNewTodoComponent } from './add-new-todo/add-new-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddNewTodoComponent
   ],
   imports: [
     BrowserModule
